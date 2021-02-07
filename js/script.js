e30c28248c3d2eda19c3133d8830e1e9e71adef9
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-07 23:37:31 +0600

    search function completed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 
 function displayMatchedItem(foodItems) {
-    console.log(foodItems);
-    console.log(foodItems.meals[0].strMeal);
-    const imageUrl = foodItems.meals[0].strMealThumb;
-    const foodItem = document.createElement('div');
-    const foodItemPrimaryInfo = `
-    <img src='${imageUrl}' alt=''>
-    <h3>${foodItems.meals[0].strMeal}</h3>
-    `
-    foodItem.className = 'food-item';
-    foodItem.innerHTML = foodItemPrimaryInfo;
-    document.getElementById('search-result-section').appendChild(foodItem);
 
+    foodItems.meals.forEach(meal => {
+        const imageUrl = meal.strMealThumb;
+        const foodItem = document.createElement('div');
+        const foodItemPrimaryInfo = `
+    <img src='${imageUrl}' alt=''>
+    <h3>${meal.strMeal}</h3>
+    `
+        foodItem.className = 'food-item';
+        foodItem.innerHTML = foodItemPrimaryInfo;
+        document.getElementById('search-result-section').appendChild(foodItem);
+    });
 }
 
 
 
 
+
+
+
+
+
+// searching-function
 function searchThefoodList() {
     const searchInput = document.getElementById('search-input').value;
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchInput}`)
