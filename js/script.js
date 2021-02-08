b110ef95152b185274e5aa0f950f877d6e705502
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-02-08 20:41:09 +0600

    final submition

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-
+// matching food item display function
 function displayMatchedItem(foodItems) {
-    console.log(foodItems);
-    foodItems.meals.forEach(meal => {
-        const imageUrl = meal.strMealThumb;
-        const foodItem = document.createElement('div');
-        const foodItemPrimaryInfo = `
-    <img src='${imageUrl}' alt=''>
-    <h3>${meal.strMeal}</h3>
-    `
-        foodItem.className = 'food-item';
-        foodItem.innerHTML = foodItemPrimaryInfo;
-        document.getElementById('search-result-section').appendChild(foodItem);
-    });
+    if (foodItems.meals != null) {
+        foodItems.meals.forEach(meal => {
+            const imageUrl = meal.strMealThumb;
+            const foodItem = document.createElement('div');
+            const foodItemPrimaryInfo = `
+        <img src='${imageUrl}' alt=''>
+        <h3>${meal.strMeal}</h3>
+        `
+            foodItem.className = 'food-item';
+            foodItem.innerHTML = foodItemPrimaryInfo;
+            document.getElementById('search-result-section').appendChild(foodItem);
+        });
+    }
+    else {
+        const searchResultSection = document.getElementById('search-result-section');
+        const NotfoundMessage = document.createElement('h3');
+        NotfoundMessage.innerText = "This food is not found";
+        searchResultSection.appendChild(NotfoundMessage);
+    }
 }
 
-
+// search button event handler
 const searchedFoodItem = document.getElementById('search-result-section');
 searchedFoodItem.addEventListener('click', event => {
     const singleFoodItemDiv = event.target.parentNode.childNodes;
@@ -26,13 +33,8 @@ searchedFoodItem.addEventListener('click', event => {
 
 })
 
-
-
-
-
+//show single selected item details function
 function showDetails(data) {
-    console.log(data);
-    console.log(data.meals[0].strMeal);
     const ingredientUl = document.createElement('ul');
     // searchedFoodItem.style.display = 'none';
     const detailsDiv = document.createElement('div');
@@ -50,20 +52,17 @@ function showDetails(data) {
         const ingredientsMeasurements = `strMeasure${i}`;
         const ingredientMeasurementIndividual = foodInfo[ingredientsMeasurements];
 
-        if (ingredientName === '' || ingredientMeasurementIndividual ==='') {
+        if (ingredientName === '' || ingredientMeasurementIndividual === '') {
             break;
         }
         const li = document.createElement('li');
         li.innerText = `Ingredient ${i}: ${ingredientMeasurementIndividual} ${ingredientName}`;
         ingredientUl.appendChild(li);
-        console.log(`Ingredient ${i}: ${ingredientName}`);
     }
     detailsDiv.appendChild(ingredientUl);
     document.getElementById('search-result-section').style.display = 'none';
     document.getElementById('details-section').appendChild(detailsDiv);
-
 }
-
 
 // searching-function
 function searchThefoodList() {
